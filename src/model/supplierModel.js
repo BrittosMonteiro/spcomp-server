2a5e00b919df0b31ee4efadf589c8e3d0cdf29ec
brittosmonteiro <brittosmonteiro@gmail.com>
2023-02-12 18:19:30 -0300

    Corrigindo modelo de fornecedore

--- src/model/supplierModel.js
+++ src/model/supplierModel.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const supplierSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  surname: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -11,7 +10,7 @@ const supplierSchema = new mongoose.Schema({
   role: { type: Number, required: true },
   contact: { type: String },
   observation: { type: String },
-  isDeleted: { type: String, required: true, default: false },
+  isDeleted: { type: Boolean, required: true, default: false },
 });
 
 const SupplierModel = mongoose.model("supplier", supplierSchema);
