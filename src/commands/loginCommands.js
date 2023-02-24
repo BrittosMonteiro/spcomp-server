468eca05584bb740abe3d02d20d8dabd572bd8b8
brittosmonteiro <brittosmonteiro@gmail.com>
2023-02-24 11:58:11 -0300

    Adiciona o nome do fornecedor e do usuário ao retorno

--- src/commands/loginCommands.js
+++ src/commands/loginCommands.js
@@ -1,6 +1,7 @@
 export function loginCommand(data) {
   const loginData = {
     username: data.username,
+    name: data.name,
     isAdmin: data.isAdmin,
     token: data._id,
     role: data.role,
